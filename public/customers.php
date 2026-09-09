c9cde0b6da0607bf56183dd8374e563e1786536d
sturyczbd02 <78055378+sturyczbd02@users.noreply.github.com>
2026-09-09 17:00:06 -0500

    customers polish

--- public/customers.php
+++ public/customers.php
@@ -17,6 +17,8 @@ $customers = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <h1>Customer List</h1>
     <a href="/dashboard.php" class="back-btn">← Back to Dashboard</a>
 
+    <br>
+
     <a href="add_customer.php">Add New Customer</a>
     <br><br>
 
