d00b55313b182cb55bf50f95faf61089e18002ca
sturyczbd02 <78055378+sturyczbd02@users.noreply.github.com>
2026-09-15 14:51:16 -0500

    add_customers fix pt 3

--- public/add_customer.php
+++ public/add_customer.php
@@ -1,9 +1,10 @@
 <?php
-
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
+?>
 
+<?php
 require_once __DIR__ . '/../includes/db.php';
 require_once __DIR__ . '/../includes/auth.php';
 require_once __DIR__ . '/../includes/permissions.php';
